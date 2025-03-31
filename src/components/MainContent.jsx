f0af45b0176b804585c01d7ad537370169ff1e4a
kmpronina <kmpronina@gmail.com>
2025-03-31 14:49:41 +0800

    Added comment

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import HelpArea from './HelpArea';
+import HelpArea from "./HelpArea";
+
+// Just a comment
 
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
@@ -11,7 +13,7 @@ function MainContent() {
 
   return (
     <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
+      <button onClick={toggleHelp}>{helpVisible ? "Hide" : "Show"} Help</button>
       {helpVisible && <HelpArea />}
     </main>
   );
